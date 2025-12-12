4b82f0fcea5db6b96979ff56c909120e93a32956
Mxdhansh <kokkonmh@mail.uc.edu>
2025-12-12 14:18:03 -0500

    Made PlayerContext file

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import './App.css'
+import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+
+// Pages (we will create these next)
+import Hub from './pages/Hub.jsx';
+import RPS from './pages/RPS.jsx';
+import TicTacToe from './pages/TicTacToe.jsx';
+import Wordle from './pages/Wordle.jsx';
+import MemoryGame from './pages/MemoryGame.jsx'; // your 4th game
 
 function App() {
-
   return (
-    <>
-      <h1>GameHub</h1>
-      <p>Welcome to GameHub</p>
-    </>
-  )
+    <div id="app">
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Hub />} />
+        <Route path="/rps" element={<RPS />} />
+        <Route path="/tictactoe" element={<TicTacToe />} />
+        <Route path="/wordle" element={<Wordle />} />
+        <Route path="/memory" element={<MemoryGame />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
